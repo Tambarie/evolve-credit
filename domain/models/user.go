7593026b5f22d3cec703b0d9832eadb64898ddc2
tambarie <gt.gbaragbo@gmail.com>
2022-06-25 01:34:06 +0100

    Fix : change hash password secret key to hash password

--- domain/models/user.go
+++ domain/models/user.go
@@ -2,12 +2,13 @@ package models
 
 // User struct
 type User struct {
-	ID              string `json:"reference,omitempty" bson:"reference"`
-	FirstName       string `json:"first_name,omitempty" bson:"first_name""`
-	LastName        string `json:"last_name,omitempty" bson:"last_name""`
-	Email           string `json:"email,omitempty" bson:"email"`
-	Password        string `json:"-,omitempty"bson:"password"`
-	HashedSecretKey string `json:"-,omitempty"bson:"hashed_secret_key"`
-	DateOfBirth     string `json:"date_of_birth" bson:"date_of_birth"`
-	CreatedAt       string `json:"created_at" bson:"created_at"`
+	ID             string `json:"id,omitempty" bson:"id"`
+	FirstName      string `json:"first_name,omitempty" bson:"first_name""`
+	LastName       string `json:"last_name,omitempty" bson:"last_name""`
+	Email          string `json:"email,omitempty" bson:"email"`
+	Password       string `json:"-,omitempty"bson:"password"`
+	HashedPassword string `json:"-,omitempty"bson:"hashed_secret_key"`
+	DateOfBirth    string `json:"date_of_birth" bson:"date_of_birth"`
+	CreatedAt      string `json:"created_at" bson:"created_at"`
+	UpdateAt       string `json:"update_at" bson:"update_at"`
 }
