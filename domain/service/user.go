f04440e0de6670eac839c18d88cb008384f3d416
tambarie <gt.gbaragbo@gmail.com>
2022-06-25 01:34:55 +0100

    Feature : method signatures to UserService interface

--- domain/service/user.go
+++ domain/service/user.go
@@ -3,14 +3,18 @@ package service
 import (
 	"github.com/Tambarie/evolve-credit/application/userRepo"
 	"github.com/Tambarie/evolve-credit/domain/models"
+	"time"
 )
 
 // UserService interface
 type UserService interface {
+	FindUserByEmail(email string) (*models.User, error)
+	FindUserByID(id string) (*models.User, error)
 	CreateUser(userDto *models.User) (*models.User, error)
-	UpdateUser(userDto *models.User) (*models.User, error)
-	GetUserByEmail(email string) ([]*models.User, error)
-	GetUserByDate(date string) ([]*models.User, error)
+	UpdateUser(userID string, update *models.UpdateUserInfo) (*models.User, error)
+	GetUserByDate(firstdate, seconddate time.Time) ([]models.User, error)
+	GetUserDetailsByEmail(email string) (*models.User, error)
+	GetAllUsers(limit int) ([]*models.User, error)
 }
 
 // UserServiceRepository struct
@@ -28,14 +32,26 @@ func (u *UserServiceRepository) CreateUser(userDto *models.User) (*models.User, 
 	return u.repo.CreateUser(userDto)
 }
 
-func (u *UserServiceRepository) GetUserByEmail(email string) ([]*models.User, error) {
-	return u.repo.GetUserByEmail(email)
+func (u *UserServiceRepository) FindUserByEmail(email string) (*models.User, error) {
+	return u.repo.FindUserByEmail(email)
 }
 
-func (u *UserServiceRepository) UpdateUser(userDto *models.User) (*models.User, error) {
-	return u.repo.UpdateUser(userDto)
+func (u *UserServiceRepository) UpdateUser(userID string, update *models.UpdateUserInfo) (*models.User, error) {
+	return u.repo.UpdateUser(userID, update)
 }
 
-func (u *UserServiceRepository) GetUserByDate(date string) ([]*models.User, error) {
-	return u.repo.GetUserByDate(date)
+func (u *UserServiceRepository) GetUserByDate(firstdate, seconddate time.Time) ([]models.User, error) {
+	return u.repo.GetUserByDate(firstdate, seconddate)
+}
+
+func (u *UserServiceRepository) GetUserDetailsByEmail(email string) (*models.User, error) {
+	return u.repo.GetUserDetailsByEmail(email)
+}
+
+func (u *UserServiceRepository) FindUserByID(id string) (*models.User, error) {
+	return u.repo.FindUserByID(id)
+}
+
+func (u *UserServiceRepository) GetAllUsers(limit int) ([]*models.User, error) {
+	return u.repo.GetAllUsers(limit)
 }
