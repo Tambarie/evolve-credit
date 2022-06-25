4850d239988e0036dd722b7e5bd4bfd2f62cedd8
tambarie <gt.gbaragbo@gmail.com>
2022-06-25 01:31:59 +0100

    Fix: modify json response function

--- response/response.go
+++ response/response.go
@@ -2,15 +2,18 @@ package response
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"time"
 )
 
-func JSON(c *gin.Context, status int, data interface{}, errs []string, message string) {
-	c.JSON(status, gin.H{
-		"status":    status,
+func JSON(c *gin.Context, message string, status int, data interface{}, errs []string) {
+	response := gin.H{
+		"message":   message,
 		"data":      data,
 		"errors":    errs,
-		"message":   message,
+		"status":    http.StatusText(status),
 		"timestamp": time.Now().Format("2006-01-02 15:04:05"),
-	})
+	}
+
+	c.JSON(status, response)
 }
