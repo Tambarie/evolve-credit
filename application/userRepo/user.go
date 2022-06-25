1e49f771bcce052d4afc37b1b8f7845110550d81
tambarie <gt.gbaragbo@gmail.com>
2022-06-25 01:33:00 +0100

    Feature: FindUserByID

--- application/userRepo/user.go
+++ application/userRepo/user.go
@@ -2,12 +2,16 @@ package userRepo
 
 import (
 	"github.com/Tambarie/evolve-credit/domain/models"
+	"time"
 )
 
 // Repository interface
 type Repository interface {
 	CreateUser(userDto *models.User) (*models.User, error)
-	UpdateUser(userDto *models.User) (*models.User, error)
-	GetUserByEmail(email string) ([]*models.User, error)
-	GetUserByDate(date string) ([]*models.User, error)
+	UpdateUser(userID string, update *models.UpdateUserInfo) (*models.User, error)
+	FindUserByEmail(email string) (*models.User, error)
+	GetUserByDate(firstdate, seconddate time.Time) ([]models.User, error)
+	GetUserDetailsByEmail(email string) (*models.User, error)
+	FindUserByID(id string) (*models.User, error)
+	GetAllUsers(limit int) ([]*models.User, error)
 }
